d1c1e33755eb2ffabbb188ea6085ba54ea395d61
BornInWinter <1714783@aluno.univesp.br>
2018-05-19 19:37:28 -0300

    Avaliação

---
+++ 3-Atividades/Semana3/Avaliacao.java
@@ -0,0 +1,110 @@
+public class Avaliacao {
+
+	public static double convert(int conv, double temp) {
+
+		switch (conv) {
+
+			case 1 :
+				return ((temp - 32) / 9) * 5;
+
+			case 2 :
+				return ((temp / 5) * 9) - 32;
+
+			case 3 :
+				return temp + 273;
+
+			case 4 :
+				return temp - 273;
+
+			case 5 :
+				return (((temp - 32) / 9) * 5) - 273;
+
+			case 6 :
+				return (((temp - 273) / 5) * 9) - 32;
+
+			default :				
+				return Double.NaN;
+
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println(convert(1, 95.5));
+
+	}
+
+}
+
+/* Método mais avançado
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Avaliacao {
+
+	static Scanner scan = new Scanner(System.in);
+
+	public static double getInput() {
+	
+		try {
+		
+			System.out.println("Qual conversão deseja realizar?\n1 Celsius para Fahrenheit\t2 Fahrenheit para Celsius\n3 Celsius para Kelvin\t\t4 Kelvin para Celsius\n5 Kelvin para Fahrenheit\t6 Fahrenheit para Kelvin");
+			int conv = scan.nextInt();
+
+			System.out.print("\nQual o valor da temperatura? ");
+			double temp = scan.nextDouble();
+
+			return convert(conv, temp);
+
+		} catch(InputMismatchException exp) {
+
+			System.err.println("\nVocê não deve usar caracteres ou pontos isolados\n");
+			scan.next();
+
+			return getInput();
+
+		}
+
+		
+	}	
+
+	public static double convert(int conv, double temp) {
+
+		switch (conv) {
+
+			case 1 :
+				return ((temp - 32) / 9) * 5;
+
+			case 2 :
+				return ((temp / 5) * 9) - 32;
+
+			case 3 :
+				return temp + 273;
+
+			case 4 :
+				return temp - 273;
+
+			case 5 :
+				return (((temp - 32) / 9) * 5) - 273;
+
+			case 6 :
+				return (((temp - 273) / 5) * 9) - 32;
+
+			default :
+				System.err.println("\nEscolha um valor dentre 1-6 correspondendo com a conversão desejada\n");
+				getInput();
+				
+				return -1;
+
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println(getInput());
+		
+	}
+
+}*/
